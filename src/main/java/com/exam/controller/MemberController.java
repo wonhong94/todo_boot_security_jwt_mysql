bd8d4ce904ba45dcd1e4c78bdf061f4c3c388952
wonhong1994 <wonhong1994@naver.com>
2024-08-07 13:08:35 +0900

    123

--- src/main/java/com/exam/controller/MemberController.java
+++ src/main/java/com/exam/controller/MemberController.java
@@ -32,7 +32,7 @@ public class MemberController {
 	@GetMapping("/")
 	public String main( ) {
 		
-		return "main home- SCM Poll 실습 Hello";
+		return "main home- SCM Poll 실습 Hello123";
 	}
 	
 	@GetMapping("/hello")
